cc1b87a6f8258e7fa24895d5472198ec5107100d
remojansen <remo.jansen@wolksoftware.com>
2017-12-02 09:59:20 +0000

    WIP Test

--- test/auth_provider.test.ts
+++ test/auth_provider.test.ts
@@ -1,7 +1,9 @@
+import * as express from "express";
 import { expect } from "chai";
 import { createApp } from "zafiro";
 import * as request from "supertest";
 import { Container } from "inversify";
+import { getManager } from "typeorm";
 import { interfaces } from "inversify-express-utils";
 import { bindings } from "../src/config/ioc_config";
 import { expressConfig } from "../src/config/express_config";
@@ -13,38 +15,52 @@ const MockAccountNotAuthenticatedRepository = accountRepositoryMockFactory({
     details: null,
     isAuthenticated: false,
     isResourceOwner: false,
-    isInRole: true,
+    isInRole: true
 });
 
 describe("Auth Provider", () => {
 
-    it("Should return 401 Unauthorized if no token is provided", async () => {
+    it("Should return 401 Unauthorized if no token is provided", (done) => {
 
-        const app = await createApp({
-            database: "postgres",
-            containerModules: [bindings],
-            AccountRepository: MockAccountNotAuthenticatedRepository,
-            expressConfig: expressConfig
-        });
+        (async () => {
 
-        type PostKeys = keyof Post;
-        type NewPost = Pick<Post, "userId"|"title"|"content"|"createdDate">;
+            try {
+                
+                const result = await createApp({
+                    database: "postgres",
+                    containerModules: [bindings],
+                    AccountRepository: MockAccountNotAuthenticatedRepository,
+                    expressConfig: expressConfig
+                });
+        
+                type PostKeys = keyof Post;
+                type NewPost = Pick<Post, "userId"|"title"|"content"|"createdDate">;
+        
+                const expectedPost: NewPost = {
+                    userId: 1,
+                    title: "Test Title",
+                    content: "Test Content",
+                    createdDate: new Date()
+                };
+        
+                const res = await httPost<NewPost>(
+                    result.app,
+                    "/api/v1/posts/",
+                    expectedPost,
+                    null,
+                    401,
+                    [["Content-Type", "text/html; charset=utf-8"]]
+                );
+    
+            } finally {
+                if (getManager().connection.isConnected) {
+                    getManager().connection.close();
+                }
+                done();
+            }
 
-        const expectedPost: NewPost = {
-            userId: 1,
-            title: "Test Title",
-            content: "Test Content",
-            createdDate: new Date()
-        };
+        })();
 
-        const res = await httPost<NewPost>(
-            app,
-            "/api/v1/posts/",
-            expectedPost,
-            null,
-            401,
-            [["Content-Type", "text/html; charset=utf-8"]]
-        );
     });
 
 });
--- test/post_controller.test.ts
+++ test/post_controller.test.ts
@@ -1,7 +1,9 @@
+import * as express from "express";
 import { expect } from "chai";
 import { createApp } from "zafiro";
 import * as request from "supertest";
 import { Container } from "inversify";
+import { getManager } from "typeorm";
 import { interfaces } from "inversify-express-utils";
 import { bindings } from "../src/config/ioc_config";
 import { expressConfig } from "../src/config/express_config";
@@ -13,41 +15,51 @@ const MockAccountNotAuthenticatedRepository = accountRepositoryMockFactory({
     details: null,
     isAuthenticated: false,
     isResourceOwner: false,
-    isInRole: true,
+    isInRole: true
 });
 
 describe("Post Controller", () => {
 
-    it("Should not be able to create a Post if not authenticated", async () => {
+    it("Should not be able to create a Post if not authenticated", (done) => {
 
-        const app = await createApp({
-            database: "postgres",
-            containerModules: [bindings],
-            AccountRepository: MockAccountNotAuthenticatedRepository,
-            expressConfig: expressConfig
-        });
+        (async () => {
+            try {
+                
+                const result = await createApp({
+                    database: "postgres",
+                    containerModules: [bindings],
+                    AccountRepository: MockAccountNotAuthenticatedRepository,
+                    expressConfig: expressConfig
+                });
+    
+                type PostKeys = keyof Post;
+                type NewPost = Pick<Post, "userId"|"title"|"content"|"createdDate">;
+    
+                const expectedPost: NewPost = {
+                    userId: 1,
+                    title: "Test Title",
+                    content: "Test Content",
+                    createdDate: new Date()
+                };
+    
+                const res = await httPost<NewPost>(
+                    result.app,
+                    "/api/v1/posts/",
+                    expectedPost,
+                    null,
+                    401,
+                    [["Content-Type", "text/html; charset=utf-8"]]
+                );
+    
+                
+            } finally {
+                if (getManager().connection.isConnected) {
+                    getManager().connection.close();
+                }
+                done();
+            }
 
-        type PostKeys = keyof Post;
-        type NewPost = Pick<Post, "userId"|"title"|"content"|"createdDate">;
-
-        const expectedPost: NewPost = {
-            userId: 1,
-            title: "Test Title",
-            content: "Test Content",
-            createdDate: new Date()
-        };
-
-        const res = await httPost<NewPost>(
-            app,
-            "/api/v1/posts/",
-            expectedPost,
-            null,
-            403,
-            [['Content-Type', "json"]]
-        );
-
-        res.body.should.have.property('participant');
-        res.body.participant.should.have.property('nuid', '98ASDF988SDF89SDF89989SDF9898');
+        })();
 
     });
 
