9c105f246f218643b6d11836119133655e4652b0
remojansen <remo.jansen@wolksoftware.com>
2017-12-02 20:20:02 +0000

    WIP test

--- test/auth_provider.test.ts
+++ test/auth_provider.test.ts
@@ -5,62 +5,52 @@ import { createApp } from "zafiro";
 import * as request from "supertest";
 import { Container } from "inversify";
 import { getManager } from "typeorm";
-import { interfaces } from "inversify-express-utils";
 import { bindings } from "../src/config/ioc_config";
 import { expressConfig } from "../src/config/express_config";
 import { accountRepositoryMockFactory } from "./account_repository.mock";
 import Post from "../src/entities/post";
 import { httPost } from "./test_utils";
-
-const MockAccountNotAuthenticatedRepository = accountRepositoryMockFactory({
-    details: null,
-    isAuthenticated: false,
-    isResourceOwner: false,
-    isInRole: true
-});
+import * as interfaces from "../src/interfaces";
 
 describe("Auth Provider", () => {
 
-    it("Should return 401 Unauthorized if no token is provided", (done) => {
+    afterEach(() => {
+        if (getManager().connection.isConnected) {
+            getManager().connection.close();
+        }
+    });
 
-        (async () => {
+    it("Should return 401 Unauthorized if no token is provided", async () => {
 
-            try {
+        const MockAccountNotAuthenticatedRepository = accountRepositoryMockFactory({
+            details: null,
+            isAuthenticated: false,
+            isResourceOwner: false,
+            isInRole: true
+        });
 
-                const result = await createApp({
-                    database: "postgres",
-                    containerModules: [bindings],
-                    AccountRepository: MockAccountNotAuthenticatedRepository,
-                    expressConfig: expressConfig
-                });
+        const result = await createApp({
+            database: "postgres",
+            containerModules: [bindings],
+            AccountRepository: MockAccountNotAuthenticatedRepository,
+            expressConfig: expressConfig
+        });
 
-                type PostKeys = keyof Post;
-                type NewPost = Pick<Post, "userId"|"title"|"content"|"createdDate">;
+        const expectedPost: interfaces.NewPost = {
+            title: "Test Title",
+            content: "Test Content"
+        };
 
-                const expectedPost: NewPost = {
-                    userId: 1,
-                    title: "Test Title",
-                    content: "Test Content",
-                    createdDate: new Date()
-                };
+        const res = await httPost<interfaces.NewPost>(
+            result.app,
+            "/api/v1/posts/",
+            expectedPost,
+            null,
+            401,
+            [["Content-Type", "text/html; charset=utf-8"]]
+        );
 
-                const res = await httPost<NewPost>(
-                    result.app,
-                    "/api/v1/posts/",
-                    expectedPost,
-                    null,
-                    401,
-                    [["Content-Type", "text/html; charset=utf-8"]]
-                );
-
-            } finally {
-                if (getManager().connection.isConnected) {
-                    getManager().connection.close();
-                }
-                done();
-            }
-
-        })();
+        expect(res.body).to.eql({});
 
     });
 
--- test/post_controller.test.ts
+++ test/post_controller.test.ts
@@ -12,94 +12,77 @@ import * as interfaces from "../src/interfaces";
 
 describe("Post Controller", () => {
 
-    it("Should not be able to create a Post if not authenticated", (done) => {
+    afterEach(() => {
+        if (getManager().connection.isConnected) {
+            getManager().connection.close();
+        }
+    });
 
-        (async () => {
-            try {
+    it("Should not be able to create a Post if not authenticated", async () => {
 
-                const MockAccountNotAuthenticatedRepository = accountRepositoryMockFactory({
-                    details: null,
-                    isAuthenticated: false,
-                    isResourceOwner: false,
-                    isInRole: true
-                });
+        const MockAccountNotAuthenticatedRepository = accountRepositoryMockFactory({
+            details: null,
+            isAuthenticated: false,
+            isResourceOwner: false,
+            isInRole: true
+        });
 
-                const result = await createApp({
-                    database: "postgres",
-                    containerModules: [bindings],
-                    AccountRepository: MockAccountNotAuthenticatedRepository,
-                    expressConfig: expressConfig
-                });
+        const result = await createApp({
+            database: "postgres",
+            containerModules: [bindings],
+            AccountRepository: MockAccountNotAuthenticatedRepository,
+            expressConfig: expressConfig
+        });
 
-                const expectedPost: interfaces.NewPost = {
-                    title: "Test Title",
-                    content: "Test Content"
-                };
+        const expectedPost: interfaces.NewPost = {
+            title: "Test Title",
+            content: "Test Content"
+        };
 
-                const res = await httPost<interfaces.NewPost>(
-                    result.app,
-                    "/api/v1/posts/",
-                    expectedPost,
-                    null,
-                    401,
-                    [["Content-Type", "text/html; charset=utf-8"]]
-                );
+        const res = await httPost<interfaces.NewPost>(
+            result.app,
+            "/api/v1/posts/",
+            expectedPost,
+            null,
+            401,
+            [["Content-Type", "text/html; charset=utf-8"]]
+        );
 
-            } finally {
-                if (getManager().connection.isConnected) {
-                    getManager().connection.close();
-                }
-                done();
-            }
-
-        })();
+        expect(res.body).to.eql({});
 
     });
 
-    it("Should able to create a post if authenticated", (done) => {
+    it("Should able to create a post if authenticated", async () => {
 
-        (async () => {
+        const MockAccountIsAuthenticatedRepository = accountRepositoryMockFactory({
+            details: {
+                id: 1
+            },
+            isAuthenticated: true,
+            isResourceOwner: false,
+            isInRole: true
+        });
 
-            try {
+        const result = await createApp({
+            database: "postgres",
+            containerModules: [bindings],
+            AccountRepository: MockAccountIsAuthenticatedRepository,
+            expressConfig: expressConfig
+        });
 
-                const MockAccountIsAuthenticatedRepository = accountRepositoryMockFactory({
-                    details: {
-                        id: 1
-                    },
-                    isAuthenticated: true,
-                    isResourceOwner: false,
-                    isInRole: true
-                });
+        const expectedPost: interfaces.NewPost = {
+            title: "Test Title",
+            content: "Test Content"
+        };
 
-                const result = await createApp({
-                    database: "postgres",
-                    containerModules: [bindings],
-                    AccountRepository: MockAccountIsAuthenticatedRepository,
-                    expressConfig: expressConfig
-                });
-
-                const expectedPost: interfaces.NewPost = {
-                    title: "Test Title",
-                    content: "Test Content"
-                };
-
-                const res = await httPost<interfaces.NewPost>(
-                    result.app,
-                    "/api/v1/posts/",
-                    expectedPost,
-                    [["x-auth-token", "fake_credentials"]],
-                    500,
-                    [["Content-Type", "text/html; charset=utf-8"]]
-                );
-
-            } finally {
-                if (getManager().connection.isConnected) {
-                    getManager().connection.close();
-                }
-                done();
-            }
-
-        })();
+        const res = await httPost<interfaces.NewPost>(
+            result.app,
+            "/api/v1/posts/",
+            expectedPost,
+            [["x-auth-token", "fake_credentials"]],
+            500,
+            [["Content-Type", "text/html; charset=utf-8"]]
+        );
 
     });
 
