655d61fe2ca77ea3f1eb87e2478d6769c3db95b9
Pedro J. Molina <pjmolina@gmail.com>
2018-01-05 18:45:33 +0100

    Added proof of concept for openapi 3.0 contract generation via metadata reflection

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,26 @@
 import "reflect-metadata";
 import chalk from "chalk";
+import { Container } from "inversify";
 import { createApp } from "zafiro";
 import { bindings } from "./config/ioc_config";
 import { expressConfig } from "./config/express_config";
 import CustomAccountRepository from "./repositories/account_repository";
+import { createOpenApiSpec }  from "zafiro-openapi3";
 
 (async () => {
 
     try {
+        const container = new Container(); // Create container
+
         const result = await createApp({
             database: "postgres",
+            container: container, // Pass container
             containerModules: [bindings],
             AccountRepository: CustomAccountRepository,
             expressConfig: expressConfig
         });
+
+        await createOpenApiSpec(container, result.app, "/openapi.json"); // Use container to generate Open Api Spec
 
         result.app.listen(
             3000,
